b7751eaefabd127d4ba3ffdd5aed928b1a69b08c
maskeynihal <maskeynihal@gmail.com>
2021-06-27 00:26:55 +0545

    Add query manipulation to get random names from the given names

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
 )
 
-const port = 8000;
+func getRandomNumber(min int, max int) int {
+	return (rand.Intn(max-min) + min)
+}
 
-func main(){
-	listenAt := fmt.Sprintf(":%d", port);
+func main() {
+	config := readConfig()
 
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request){
-		fmt.Println(req)
-		fmt.Fprint(res, "Hello World")
+	listenAt := fmt.Sprintf(":%d", config.port)
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		responseFormat := req.URL.Query().Get("format")
+
+		names := strings.Split(req.URL.Query().Get("names"), ",")
+
+		randomIndex := getRandomNumber(0, len(names))
+
+		randomName := names[randomIndex]
+		fmt.Println(randomName)
+
+		if responseFormat == "json" {
+			res.Header().Set("Content-type", "application/json;charset=utf-8")
+
+			json.NewEncoder(res).Encode(struct {
+				Name string `json:"name"`
+			}{
+				Name: randomName,
+			})
+
+			return
+		}
+
+		fmt.Fprint(res, randomName)
+
 	})
 
-	log.Printf("Open the following URL in the browser: http://localhost:%d\n", port)
+	log.Printf("Open the following URL in the browser: http://localhost:%d\n", config.port)
 
 	log.Fatal(http.ListenAndServe(listenAt, nil))
 }
